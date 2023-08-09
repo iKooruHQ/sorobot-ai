1cf2a7fd8652011819a7a2642c5e45d6a19a82aa
iKooru.com <124317926+iKooruHQ@users.noreply.github.com>
2023-08-09 06:57:58 -0700

    Update Prompt

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -4,15 +4,15 @@ import { HNSWLib, SupabaseVectorStore } from 'langchain/vectorstores';
 import { PromptTemplate } from 'langchain/prompts';
 
 const CONDENSE_PROMPT =
-  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+  PromptTemplate.fromTemplate(`Given the following conversation and a follow-up question, rephrase the follow-up question to be a standalone question.
 
 Chat History:
 {chat_history}
-Follow Up Input: {question}
+Follow-Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(
-  `You are an AI assistant and a Stellar Blockchain and Soroban smart contract coding expert. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
+   `You are an AI assistant and a Stellar Blockchain and Soroban smart contract coding expert. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
   You should only use hyperlinks as references that are explicitly listed as a source in the context below. Do NOT make up a hyperlink that is not listed below.
   You're an AI versed in the Soroban smart contract platform, which utilizes Rust for contract development.
 
@@ -78,7 +78,6 @@ const QA_PROMPT = PromptTemplate.fromTemplate(
     'https://soroban.stellar.org/docs/reference/freighter',
     'https://soroban.stellar.org/docs/developer-tools',
     'https://soroban-react.gitbook.io/index/',
-
     SOROBAN REACT URLS:
   'https://soroban-react.gitbook.io/index/Technical-docs',
   'https://soroban-react.gitbook.io/index/Technical-docs/interfaces',
@@ -235,24 +234,19 @@ const QA_PROMPT = PromptTemplate.fromTemplate(
   2. Use clear and concise language for each instruction.
   3. If you need to include a sub-step or detail, use a nested list.
   
-  **Example:**
-  
+  All contracts should begin with #![no_std] to ensure that the Rust standard library is not included in the build. The Rust standard library is large and not well suited to being deployed into small programs like those deployed to blockchains.
 
+  use soroban_sdk contract, contractimpl, symbol_short, vec, Env, Symbol, Vec when building dapps use yarn create-next-app instead of create-react-app 
   
-  ---
-  
-  ### Your Instructions:
-  
-  1. 
-  2. 
-  3. 
-  
+  Many of the types available in typical Rust programs, such as std::vec::Vec, are not available. The soroban-sdk provides a variety of types like Vec, Map, Bytes, BytesN, Symbol, that all utilize the Soroban environment's memory and native capabilities. Primitive values like u128, i128, u64, i64, u32, i32, and bool can also be used. Floats and floating point math are not supported.
+ 
   Question: {question}
   =========
   {context}
   =========
-  Answer in Markdown and Show a Sample Code Block:`,
+  Answer in Markdown, link to the referenced documentation, and Show a Sample Code when requested:`,
 );
+
 
 export const makeChain = (
   vectorstore: SupabaseVectorStore,
